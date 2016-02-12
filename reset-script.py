1035d2cbbfc73e06d38f2990449bee8e56527663
Kevin Dunn <kgdunn@gmail.com>
2016-02-12 19:50:14 +0100

    Allow you to reset all counts for a particular user now

---
+++ reset-script.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+# Resets the counts and accuracy for a particular user
+import os
+import django
+import urllib2
+import csv
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+import flashcards.settings
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "flashcards.settings"
+)
+django.setup()
+
+from vocab.models import WordItem, Person
+
+person = Person.objects.filter(email='kgdunn@gmail.com')[0]
+pairs = WordItem.objects.filter(person=person)
+for item in pairs:
+    item.counts_wrong = 0
+    item.counts_right = 0
+    item.answers = ''
+    item.save
